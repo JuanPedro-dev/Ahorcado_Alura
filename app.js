f22f06a776d31a11633ff1e0dc9b09990fe0e6b2
Juan Pedro <juanpedro.developer@gmail.com>
2022-05-19 12:15:40 -0300

    safe point

--- app.js
+++ app.js
@@ -1,9 +1,153 @@
-let cuaderno = document.querySelector('.man');
-let pincel = cuaderno.getContext("2d");
+// Dibujando una hoja con canvas
+let pantalla = document.querySelector("canvas");
+let pincel = pantalla.getContext("2d");    
+pincel.fillStyle = "#ddd";
+pincel.fillRect(0,0,300,400); 
+
+pincel.fillStyle = "#aaa";
+pincel.beginPath();
+pincel.arc(25, 30, 6, 0, 2*3.14);
+pincel.fill();
+pincel.beginPath();
+pincel.arc(25, 200, 6, 0, 2*3.14);
+pincel.fill();
+pincel.beginPath();
+pincel.arc(25, 370, 6, 0, 2*3.14);
+pincel.fill();
 
 pincel.fillStyle = "red";
-pincel.FillRect(0,0,600,400);
+pincel.beginPath();
+pincel.fillRect(50,0,1,400); 
+
+for(let i=40; i<400; i+=20){
+    pincel.fillStyle = "#000";
+    pincel.fillRect(0,i,300,1); 
+}
+
+pincel.font = "15px Comic Sans MS";
+pincel.strokeText("Juan Pedro", 60, 40);
+pincel.strokeText("Alura", 250, 40);
+// Fin dibujo hoja... 
+
+
+// Dibujando el hombre - cada error una parte nueva. 
 
 
 
+// creo el conjunto de palabras.
+const arrayPalabras = ["HTML", "JavaScript", "arreglos", "css", "challenge" ]; 
 
+// arreglo con las letras ingresadas
+const letrasMalas = [];
+
+
+// Las paso a minuscula. 
+const palabras = [];
+arrayPalabras.forEach(palabra => palabras.push(palabra.toLowerCase())); 
+
+// Genero una palabra random entre las que estan. 
+const randomPalabra = arrayPalabras[Math.floor(Math.random() * arrayPalabras.length)]; 
+
+pincel.font = "30px Arial";
+
+// Dibujo la cantidad de lugares según el largo de la palabra
+for(let i = 0; i <randomPalabra.length; i++){
+    let espacios = i*30 + 60;
+    if (i < 8 ){
+        pincel.strokeText("_", espacios, 320); 
+    } else{
+        espacios = (i-8)*30 + 60;
+        pincel.strokeText("_", espacios, 360); 
+    }
+}
+let contador = 0;
+
+// Event Listener teclado 
+window.addEventListener("keydown",(event)=>{
+    let letraInput = event.key.toLocaleLowerCase();
+    let acerto = false;
+    for(let i=0; i < randomPalabra.length; i++){
+        if(letraInput == randomPalabra[i]){
+            encontro = true;
+            let espacios = i*30 + 60;
+            if (i < 8 ){
+                pincel.strokeText(letraInput, espacios, 310); 
+            } else{
+                espacios = (i-8)*30 + 60;
+                pincel.strokeText(letraInput, espacios, 350); 
+            }
+        }
+    } 
+    if(!acerto){
+        contador++; 
+        dibujar();
+    }
+
+})
+
+function dibujar(){
+        
+    switch(contador){
+        case 1: 
+            // Error 1 - pie del poste
+            pincel.strokeStyle = "blue"; 
+            pincel.lineWidth = 2;
+            pincel.moveTo(82,250);
+            pincel.lineTo(50,270);
+            pincel.stroke();
+
+            pincel.moveTo(80,250);
+            pincel.lineTo(110,270);
+            pincel.stroke();
+            break;
+        case 2: 
+            // Error 2 - poste
+            pincel.fillStyle = "blue"
+            pincel.fillRect(80,90,2,180); 
+            break;
+        case 3: 
+            // Error 3 - horizontal arriba cabeza
+            pincel.fillStyle = "blue"
+            pincel.fillRect(80,90,100,2); 
+            break;
+        case 4: 
+            // Error 4 - vertical arriba cabeza
+        pincel.fillStyle = "blue"
+        pincel.fillRect(180,90,2,30); 
+            break;
+        case 5: 
+        // Error 5 - cabeza
+        pincel.beginPath();
+        pincel.arc(182, 130, 12, 0, 2*3.14);
+        pincel.stroke();
+            break;
+        case 6: 
+        // Error 6 - cuerpo
+            pincel.fillStyle = "blue"
+            pincel.fillRect(180,140,2,60); 
+            break;
+        case 7: 
+            // Error 7 - pie izq
+            pincel.moveTo(182,200);
+            pincel.lineTo(160,220);
+            pincel.stroke();
+            break;
+        case 8: 
+            // Error 8 - pie der
+            pincel.moveTo(182,200);
+            pincel.lineTo(202,220);
+            pincel.stroke();
+            break;
+        case 9: 
+            // Error 9 -  Brazos der
+            pincel.moveTo(182,150);
+            pincel.lineTo(202,170);
+            pincel.stroke();
+            break;
+        case 10: 
+            // Error 10 -Brazos izq
+            pincel.moveTo(182,150);
+            pincel.lineTo(160,170);
+            pincel.stroke();
+    }   
+}
